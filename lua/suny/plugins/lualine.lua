c753867a20fc4ae33269a953e02974a3c7d66cef
suny <1078460760@qq.com>
2024-06-06 15:18:41 +0800

    update nvchad style

--- lua/suny/plugins/lualine.lua
+++ lua/suny/plugins/lualine.lua
@@ -3,73 +3,158 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- config = function()
+  --   local lualine = require("lualine")
+  --   -- 获得lazy插件管理器的状态
+  --   local lazy_status = require("lazy.status")
+  --
+  --   local colors = {
+  --     blue = "#65D1FF",
+  --     green = "#3EFFDC",
+  --     violet = "#FF61EF",
+  --     yellow = "#FFDA7B",
+  --     red = "#FF4A4A",
+  --     fg = "#c3ccdc",
+  --     bg = "#112638",
+  --     inactive_bg = "#2c3043",
+  --   }
+  --
+  --   local my_lualine_theme = {
+  --     normal = {
+  --       a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+  --       b = { bg = colors.bg, fg = colors.fg },
+  --       c = { bg = colors.bg, fg = colors.fg },
+  --     },
+  --     insert = {
+  --       a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+  --       b = { bg = colors.bg, fg = colors.fg },
+  --       c = { bg = colors.bg, fg = colors.fg },
+  --     },
+  --     visual = {
+  --       a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+  --       b = { bg = colors.bg, fg = colors.fg },
+  --       c = { bg = colors.bg, fg = colors.fg },
+  --     },
+  --     command = {
+  --       a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+  --       b = { bg = colors.bg, fg = colors.fg },
+  --       c = { bg = colors.bg, fg = colors.fg },
+  --     },
+  --     replace = {
+  --       a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+  --       b = { bg = colors.bg, fg = colors.fg },
+  --       c = { bg = colors.bg, fg = colors.fg },
+  --     },
+  --     inactive = {
+  --       a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+  --       b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+  --       c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+  --     },
+  --   }
+  --
+  --   -- confnigure lualine with modified theme
+  --   lualine.setup({
+  --     options = {
+  --       theme = my_lualine_theme,
+  --     },
+  --     -- lazy状态
+  --     sections = {
+  --       lualine_x = {
+  --        {
+  --         lazy_status.updates,
+  --         cond = lazy_status.has_updates,
+  --         color = { fg = "#ff9e64" },
+  --        },
+  --        { "encoding" },
+  --        { "fileformat" },
+  --        { "filetype" },
+  --       },
+  --     },
+  --   })
+  -- end,
+  -- 上面是old
+  -- nvchad 配置
   config = function()
-    local lualine = require("lualine")
-    -- 获得lazy插件管理器的状态
-    local lazy_status = require("lazy.status")
-
     local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
+      bg = "#1e222a",
+      fg = "#bbc2cf",
+      yellow = "#ECBE7B",
+      cyan = "#008080",
+      darkblue = "#081633",
+      green = "#98be65",
+      orange = "#FF8800",
+      violet = "#a9a1e1",
+      magenta = "#c678dd",
+      blue = "#51afef",
+      red = "#ec5f67",
     }
 
-    local my_lualine_theme = {
-      normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-      },
+    local conditions = {
+      buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+      end,
+      hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+      end,
+      check_git_workspace = function()
+        local filepath = vim.fn.expand("%:p:h")
+        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+      end,
     }
 
-    -- confnigure lualine with modified theme
-    lualine.setup({
+    require("lualine").setup({
       options = {
-        theme = my_lualine_theme,
-      },
-      -- lazy状态
-      sections = {
-        lualine_x = {
-         {
-          lazy_status.updates,
-          cond = lazy_status.has_updates,
-          color = { fg = "#ff9e64" },
-         },
-         { "encoding" },
-         { "fileformat" },
-         { "filetype" },
+        icons_enabled = true,
+        theme = {
+          normal = {
+            a = { fg = colors.bg, bg = colors.blue },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
+          insert = { a = { fg = colors.bg, bg = colors.green } },
+          visual = { a = { fg = colors.bg, bg = colors.magenta } },
+          replace = { a = { fg = colors.bg, bg = colors.red } },
+          inactive = {
+            a = { fg = colors.fg, bg = colors.bg },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
         },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "dashboard", "NvimTree", "packer" },
       },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          { "filename", cond = conditions.buffer_not_empty, color = { fg = colors.magenta, gui = "bold" } },
+        },
+        lualine_x = {
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " " },
+            color = { fg = colors.red },
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = {},
     })
   end,
 }
--- lua/suny/plugins/nvim-tree.lua
+++ lua/suny/plugins/nvim-tree.lua
@@ -54,5 +54,12 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+
+    -- 自动打开 nvim-tree
+    vim.api.nvim_create_autocmd({ "VimEnter" }, {
+      callback = function()
+        require("nvim-tree.api").tree.open()
+      end,
+    })
+  end,
 }
