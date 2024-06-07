461d76c4a41f10aa63721f78a7e9dee3bfa02aa1
suny <1078460760@qq.com>
2024-06-07 15:07:06 +0800

    update colorscheme mocha

--- lua/suny/plugins/colorscheme.lua
+++ lua/suny/plugins/colorscheme.lua
@@ -1,88 +1,84 @@
+-- catppuccin
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  -- config = function()
-  --   local bg = "#011628"
-  --   local bg_dark = "#011423"
-  --   local bg_highlight = "#143652"
-  --   local bg_search = "#0A64AC"
-  --   local bg_visual = "#275378"
-  --   local fg = "#CBE0F0"
-  --   local fg_dark = "#B4D0E9"
-  --   local fg_gutter = "#627E97"
-  --   local border = "#547998"
-  --
-  --   require("tokyonight").setup({
-  --     style = "night",
-  --     on_colors = function(colors)
-  --       colors.bg = bg
-  --       colors.bg_dark = bg_dark
-  --       colors.bg_float = bg_dark
-  --       colors.bg_highlight = bg_highlight
-  --       colors.bg_popup = bg_dark
-  --       colors.bg_search = bg_dark
-  --       colors.bg_sidebar = bg_dark
-  --       colors.bg_statusline = bg_dark
-  --       colors.bg_visual = bg_dark
-  --       colors.border = border
-  --       colors.fg = fg
-  --       colors.fg_dark = fg_dark
-  --       colors.fg_float = fg
-  --       colors.fg_gutter = fg_gutter
-  --       colors.fg_sidebar = fg_dark
-  --     end
-  --   })
-  --
-  --   vim.cmd("colorscheme tokyonight")
-  -- end
-  config = function()
-    require("tokyonight").setup({
-      style = "night", -- 可以选择'storm', 'night' 或 'day'
-      transparent = false, -- 透明背景
-      terminal_colors = true, -- 终端颜色
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        sidebars = "dark", -- darker background for sidebars
-        floats = "dark", -- darker background for floating windows
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dap = true,
+        dap_ui = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = true,
+        markdown = true,
+        mason = true,
+        native_lsp = { enabled = true },
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        treesitter = true,
+        ts_rainbow = false,
+        ufo = true,
+        which_key = true,
+        window_picker = true,
       },
-      -- 配置 NVChad 风格的亮度
-      on_colors = function(colors)
-        colors.border = "#565f89"
-      end,
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-      end,
-    })
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+    },
+    config = function()
+      vim.cmd("colorscheme catppuccin-mocha")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
 }
+-- old tokyonight
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   config = function()
+--     local bg = "#011628"
+--     local bg_dark = "#011423"
+--     local bg_highlight = "#143652"
+--     local bg_search = "#0A64AC"
+--     local bg_visual = "#275378"
+--     local fg = "#CBE0F0"
+--     local fg_dark = "#B4D0E9"
+--     local fg_gutter = "#627E97"
+--     local border = "#547998"
+--
+--     require("tokyonight").setup({
+--       style = "night",
+--       on_colors = function(colors)
+--         colors.bg = bg
+--         colors.bg_dark = bg_dark
+--         colors.bg_float = bg_dark
+--         colors.bg_highlight = bg_highlight
+--         colors.bg_popup = bg_dark
+--         colors.bg_search = bg_dark
+--         colors.bg_sidebar = bg_dark
+--         colors.bg_statusline = bg_dark
+--         colors.bg_visual = bg_dark
+--         colors.border = border
+--         colors.fg = fg
+--         colors.fg_dark = fg_dark
+--         colors.fg_float = fg
+--         colors.fg_gutter = fg_gutter
+--         colors.fg_sidebar = fg_dark
+--       end
+--     })
+--
+--     vim.cmd("colorscheme tokyonight")
+--   end
+-- }
--- lua/suny/plugins/telescope.lua
+++ lua/suny/plugins/telescope.lua
@@ -31,23 +31,18 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n","<leader>ff","<cmd>Telescope find_files<cr>",{ desc = "Fuzzy find files in cwd" })
-    keymap.set("n","<leader>fr","<cmd>Telescope oldfiles<cr>",{ desc = "Fuzzy find recent files" })
-    keymap.set("n","<leader>fs","<cmd>Telescope live_grep<cr>",{ desc = "Find string in cwd" })
-    keymap.set("n","<leader>fc","<cmd>Telescope grep_string<cr>",{ desc = "Find string under cursor in cwd" })
-    keymap.set("n","<leader>ft","<cmd>TodoTelescope<cr>",{ desc = "Find todos" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 
     -- 在窗口按tab可以选中需要的,然后ctrl+q唤出quick list
     -- 自定义高亮组
     -- 先通过:highlight CursorLine得到光标行的高亮
     -- 在设置guibg
-    vim.cmd [[
-      highlight TelescopeSelection guibg=#143652
-    ]]
-
-    -- 设置复制时的高亮,跟光标行高亮一致
-    vim.cmd [[
-      highlight Visual guibg=#143652
-    ]]
+    -- vim.cmd [[
+    --   highlight TelescopeSelection guibg=#143652
+    -- ]]
   end,
 }
